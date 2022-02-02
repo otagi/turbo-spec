8dfadf1416dc631673c13e3634a9396e197e33fe
Tiago Mendes-Costa <otagi@users.noreply.github.com>
2022-02-02 22:33:54 +0100

    Update counter when rodents are added

--- app/models/rodent.rb
+++ app/models/rodent.rb
@@ -1,3 +1,10 @@
 class Rodent < ApplicationRecord
   validates_presence_of :name
+  after_commit :update_rodents_counter
+
+  private
+
+  def update_rodents_counter
+    broadcast_replace_to :dashboard, target: 'rodents-counter', partial: 'dashboard/counter'
+  end
 end
