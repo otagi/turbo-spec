2834115f9ee5320ea2d8e6587f1e2127192969ec
Tiago Mendes-Costa <otagi@users.noreply.github.com>
2022-02-03 18:39:36 +0100

    Use simplecss.org stylesheet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Indent html for pretty debugging and do not sort attributes
+  Slim::Engine.set_options pretty: true, sort_attrs: false
 end
