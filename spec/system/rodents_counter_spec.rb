13fa8078f701804a646be19a7720375c245dba31
Tiago Mendes-Costa <otagi@users.noreply.github.com>
2022-02-03 17:44:54 +0100

    Add system spec for rodent deletion

--- spec/system/rodents_counter_spec.rb
+++ spec/system/rodents_counter_spec.rb
@@ -5,10 +5,12 @@ feature 'Rodents Counter', puma: true, action_cable: :inline, active_job: :inlin
     driven_by :selenium, using: :headless_chrome
   end
 
+  given!(:existing_rodent) { create(:rodent, name: 'Chipmunk') }
+
   scenario 'Creating a rodent updates the dashboard rodents counter' do
     visit '/'
 
-    expect(page).to have_text 'There are 0 rodents in the encyclopedia!'
+    expect(page).to have_text 'There are 1 rodents in the encyclopedia!'
 
     new_window = open_new_window
     within_window new_window do
@@ -17,6 +19,22 @@ feature 'Rodents Counter', puma: true, action_cable: :inline, active_job: :inlin
       click_on 'Create Rodent'
     end
 
+    expect(page).to have_text 'There are 2 rodents in the encyclopedia!'
+  end
+
+  scenario 'Deleting a rodent updates the dashboard rodents counter' do
+    visit '/'
+
     expect(page).to have_text 'There are 1 rodents in the encyclopedia!'
+
+    new_window = open_new_window
+    within_window new_window do
+      visit '/rodents'
+      accept_alert do
+        click_on 'Destroy'
+      end
+    end
+
+    expect(page).to have_text 'There are 0 rodents in the encyclopedia!'
   end
 end
